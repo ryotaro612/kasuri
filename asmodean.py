e765834c3fb9e309ccbc8736c9b487dcd5354ed9
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-05-19 08:17:59 +0900

    scrape tool page

--- asmodean.py
+++ asmodean.py
@@ -5,10 +5,16 @@ import urllib3
 from bs4 import BeautifulSoup
 http = urllib3.PoolManager()
 
+def collect_tool_page_urls(tool_idxpage_structure):
+    soup = BeautifulSoup(tool_idxpage_structure, 'html.parser')
+    return map(lambda e: e.attrs['href'], soup.find_all('table')[1].find_all('a'))
+
+def map_tool_url_tool_page(tool_page_urls):
+    tool_pages = map(lambda url: BeautifulSoup(http.request('GET', url).data, 'html.parser'), tool_page_urls)
+    # b[0].find('a').attrs['href']
+    return tool_pages
+
 r = http.request('GET', 'http://asmodean.reverse.net/pages/tools_index.html')
 
-def collect_tool_url(tool_idxpage_structure):
-    soup = BeautifulSoup(tool_idxpage_structure, 'html.parser')
-    return list(map(lambda e: e.attrs['href'], soup.find_all('table')[1].find_all('a')))
-
-print(collect_tool_url(r.data))
+tool_page_urls = collect_tool_page_urls(r.data)
+a = map_tool_url_tool_page(list(tool_page_urls)[:3])
