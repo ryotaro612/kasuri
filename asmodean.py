a3408048c0c60680e1a73bcecd583dbcdf01aee8
Sato Kazuma <sato.kazuma.kzs@gmail.com>
2016-05-19 07:42:42 +0900

    get all urls to tools

--- asmodean.py
+++ asmodean.py
@@ -2,15 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import urllib3
-
+from bs4 import BeautifulSoup
 http = urllib3.PoolManager()
 
 r = http.request('GET', 'http://asmodean.reverse.net/pages/tools_index.html')
 
-r.data
-print(asmodean.soup.find_all('table')[1])
+def collect_tool_url(tool_idxpage_structure):
+    soup = BeautifulSoup(tool_idxpage_structure, 'html.parser')
+    return list(map(lambda e: e.attrs['href'], soup.find_all('table')[1].find_all('a')))
 
-
-
-
-
+print(collect_tool_url(r.data))
