9aaa2ae430a788fadae7b570477085e48549722e
Ryotaro <ryotaro612.ooo@gmail.com>
2025-07-27 02:29:56 +0900

    Remove command line argument parsing and related functionality
    - Deleted the `parseCmdLine` function and its associated tests from `args.ts` and `args.test.ts`.
    - Removed the environment variable file `src/env` that contained OIDC credentials.
    - Eliminated the `signIn` function and its dependencies from `signin.ts` and updated `main.ts` to remove related logic.

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,5 @@
-import { parseCmdLine } from "./args.ts";
-import { signIn } from "./signin.ts";
 
 export async function main(args: string[]): Promise<number> {
-  const command = parseCmdLine(args);
-  if (command.error) {
-    console.error(command.error);
-    return 1;
-  }
-  await signIn({
-    clientId: command.oidcClientId,
-    clientSecret: command.clientSecret,
-    port: command.port,
-  });
+
   return 0;
 }
