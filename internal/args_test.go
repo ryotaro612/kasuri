8eda07e7728c688cf0c5ae0375452c2adb471b4e
Ryotaro <ryotaro612.ooo@gmail.com>
2025-08-23 14:48:02 +0900

    a

--- internal/args_test.go
+++ internal/args_test.go
@@ -4,42 +4,6 @@ import (
 	"testing"
 )
 
-func TestVerboseIsBoolOption(t *testing.T) {
-	t.Parallel()
-	args := []string{"-verbose", "-token", "a"}
-	actual, err := Read(args)
-	if err != nil {
-		t.Errorf("Expected no error, got %v", err)
-	}
-
-	expected := Args{
-		verbose: true,
-		token:   "a",
-	}
-
-	if expected != actual {
-		t.Errorf("The expected value is %#v, but got %#v", expected, actual)
-	}
-
-}
-
-func TestVerboseIsOptional(t *testing.T) {
-	t.Parallel()
-	actual, err := Read([]string{"-token", "a"})
-	if err != nil {
-		t.Errorf("Expected no error, got %v", err)
-	}
-
-	expected := Args{
-		verbose: false,
-		token:   "a",
-	}
-	if expected != actual {
-		t.Errorf("The expected value is %#v, but got %#v", expected, actual)
-	}
-
-}
-
 func TestSlackTokenCanBePassedAsEnvVar(t *testing.T) {
 	t.Setenv("ASHITABA_SLACK_TOKEN", "test_token")
 	cmd, err := Read([]string{})
@@ -49,4 +13,49 @@ func TestSlackTokenCanBePassedAsEnvVar(t *testing.T) {
 	if cmd.token != "test_token" {
 		t.Error("cmd.token should be set to the value of ASHITABA_SLACK_TOKEN")
 	}
+}
+
+func TestCommandLineArgumentSyntax(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name     string
+		args     []string
+		expected Args
+	}{
+		{
+			name: "verbose flag is acceptable",
+			args: []string{"-verbose", "-token", "a"},
+			expected: Args{
+				verbose: true,
+				token:   "a",
+			},
+		},
+		{
+			name: "verbose flag is optional",
+			args: []string{"-token", "a"},
+			expected: Args{
+				verbose: false,
+				token:   "a",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			actual, err := Read([]string{"-token", "a"})
+			if err != nil {
+				t.Errorf("Expected no error, got %v", err)
+			}
+
+			expected := Args{
+				verbose: false,
+				token:   "a",
+			}
+			if expected != actual {
+				t.Errorf("The expected value is %#v, but got %#v", expected, actual)
+			}
+		})
+	}
+
 }
